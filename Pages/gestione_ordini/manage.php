1884fdf803faffe3eb29533f1501a2b5e3c8a7d3
arevaloapol.lor.2007 <lorenzo.arevalo@outlook.it>
2026-03-05 12:07:16 +0100

    Fix percorsi manage.php Stavolta per davvero (si spera)

--- Pages/gestione_ordini/manage.php
+++ Pages/gestione_ordini/manage.php
@@ -28,7 +28,7 @@ if (!isset($_GET["id"])) {
                 border-radius: 4px;
             }
         </style>
-        <link rel="stylesheet" href="./Assets/Styles/style.css">
+        <link rel="stylesheet" href="../../Assets/Styles/style.css">
     </head>
     <body>
 
@@ -36,12 +36,12 @@ if (!isset($_GET["id"])) {
         <div class="nav-container">
     
             <div class="brand">
-                <img src="./Assets/Images/logo.png" alt="Logo Speedy Break">
+                <img src="../../Assets/Images/logo.png" alt="Logo Speedy Break">
                 <span>Speedy Break</span>
             </div>
     
             <ul class="nav-links">
-                <li><a class="active" href="index.html">Home</a></li>
+                <li><a class="active" href="../../index.html">Home</a></li>
                 <li><a href="./Pages/creazione_ordine/ordine.html">Ordina</a></li>
                 <li><a href="./Pages/gestione_ordini/manage.php">Gestione Ordini</a></li>
                 <li><a href="./Pages/amministrazione/admin.php">Admin</a></li>

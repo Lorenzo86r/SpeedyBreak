78486c8f6513df39e9eebd171526965f5bf75bbe
arevaloapol.lor.2007 <lorenzo.arevalo@outlook.it>
2026-03-05 11:24:12 +0100

    Modifica Push  manage.php Lorenzo Arevalo 05/03/26

--- Pages/gestione_ordini/manage.php
+++ Pages/gestione_ordini/manage.php
@@ -2,7 +2,8 @@
 require_once "gestione-ordine.php";
 
 /* connessione al database  */
-$db = new Database("localhost", "", "root", "");
+$db   = 'my_saqlain';
+$db = new Database("localhost", "my_saqlain", "root", "");
 $message = "";
 
 

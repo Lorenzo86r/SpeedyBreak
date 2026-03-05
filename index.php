e2e85a47fe5ee85ce754fd05733c16972f945a94
luca <luca.bisognin13@gmail.com>
2026-03-05 11:11:04 +0100

    update orders 1.2

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home - Gestione Ordini</title>
 
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="Assets/Styles/style.css">
 </head>
 <body>
 

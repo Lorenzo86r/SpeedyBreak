79ca1f324d8c4e232a55eb7e96b568f9e7c333be
notSaqlain <117275944+notSaqlain@users.noreply.github.com>
2026-03-12 11:30:55 +0100

    Update admin.php

--- Pages/amministrazione/admin.php
+++ Pages/amministrazione/admin.php
@@ -8,7 +8,7 @@ if (!isset($_SESSION["ruolo"]) || $_SESSION["ruolo"] !== 'admin') {
 $host = "localhost";
 $user = "root";
 $pass = "";
-$db = "my_input789";
+$db = "my_saqlain";
 
 $conn = new mysqli($host, $user, $pass, $db);
 if ($conn->connect_error) die("Connessione fallita: " . $conn->connect_error);

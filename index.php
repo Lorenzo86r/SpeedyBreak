d627218d7d21567388a12b4ff75d6764c10ae5c7
notSaqlain <iamnotsaqlain@gmail.com>
2026-03-12 13:11:51 +0100

    fix logo

--- index.php
+++ index.php
@@ -39,7 +39,7 @@
                             </svg>
                         </a>
                     <?php else: ?>
-                        <a class="nav-icon-btn" href="Pages/auth/login.php" title="Login">
+                        <a class="nav-icon-btn" href="Pages/auth/login.php" title="Accedi">
                             <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                                 <path d="M15 3h4a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2h-4"></path>
                                 <polyline points="10 17 15 12 10 7"></polyline>

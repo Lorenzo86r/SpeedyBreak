4800dd74b62fbef8213f7c0bd3ee9dddbebc393e
luca <luca.bisognin13@gmail.com>
2026-03-19 11:50:35 +0100

    update orders

--- Pages/creazione_ordine/index_order.php
+++ Pages/creazione_ordine/index_order.php
@@ -324,7 +324,7 @@ $result = $conn->query($sql);
 
                 document.getElementById('no-results').style.display = visible === 0 ? 'block' : 'none';
             }
-            const userSaldo = <?= $user_saldo ?? 0 ?>;
+            const userSaldo = <?= $user_saldo ?? 100 ?>;
         </script>
     </body>
 </html>
